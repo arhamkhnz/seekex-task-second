245abd4504c54df87eb5768f3d8b8ffd0f0307a9
arhamkhnz <md.arhamkhan09@gmail.com>
2023-03-30 21:38:30 +0530

    Added Edit Functionality UI & Warning Fixes

--- src/components/CardListComponent.js
+++ src/components/CardListComponent.js
@@ -1,29 +1,45 @@
-import React, { useState, useEffect } from "react";
-import { Card, Modal, Row, Col, Image } from "antd";
-import { EditOutlined, EyeOutlined } from "@ant-design/icons";
+import React, { useState } from "react";
+import { Card, Modal, Row, Col, Image, Input, Button, Select } from "antd";
+import { EyeOutlined } from "@ant-design/icons";
 
 const { Meta } = Card;
 
 export default function CardListComponent({ person }) {
   const [visible, setVisible] = useState(false);
-  const [selectedPerson, setSelectedPerson] = useState(null);
+  const [edit, setEdit] = useState(false);
+  const [editValues, setEditValues] = useState({
+    gender: person.gender,
+    phone: person.phone,
+    age: person.age,
+    bloodGroup: person.bloodGroup,
+  });
 
-  const handleCardClick = (person) => {
+  const handleCardClick = () => {
     setVisible(true);
   };
 
   const handleModalClose = () => {
     setVisible(false);
+    setEdit(false)
+    setEditValues({
+      gender: person.gender,
+      phone: person.phone,
+      age: person.age,
+      bloodGroup: person.bloodGroup,
+    })
+  };
+
+  const handleEditValueChange = (key, value) => {
+    setEditValues({
+      ...editValues,
+      [key]: value,
+    });
   };
 
   return (
     <>
       <Col key={person.id} xs={24} sm={12} md={8} lg={6}>
-        <Card
-          hoverable
-          className="card-view"
-          onClick={() => handleCardClick(person)}
-        >
+        <Card hoverable className="card-view">
           <Row>
             <Col xs={8} sm={6} md={6}>
               <div className="avatar-wrapper">
@@ -40,17 +56,31 @@ export default function CardListComponent({ person }) {
                 title={person.firstName}
                 description={person.email}
               />
-              <button
-                className="view-button"
-                onClick={() => handleCardClick(person)}
-              >
+              <button className="view-button" onClick={() => handleCardClick()}>
                 <EyeOutlined />
               </button>
             </Col>
           </Row>
         </Card>
       </Col>
-      <Modal open={visible} onCancel={handleModalClose} footer={null}>
+      <Modal
+        open={visible}
+        onCancel={handleModalClose}
+        footer={
+          edit ? (
+            <div className="modal-footer-edit">
+              <Button onClick={() => setEdit(false)}>Cancel</Button>
+              <Button onClick={() => console.log(editValues)} type="primary">
+                Update
+              </Button>
+            </div>
+          ) : (
+            <div className="modal-footer">
+              <Button onClick={() => setEdit(true)}>Edit</Button>
+            </div>
+          )
+        }
+      >
         <div className="modal-content">
           <div className="modal-avatar-wrapper">
             <img
@@ -64,26 +94,89 @@ export default function CardListComponent({ person }) {
           </div>
           <div className="modal-details">
             <div className="modal-email">{person.email}</div>
-            <div className="modal-other-details">
-              <div className="modal-other-detail">
-                <div className="modal-other-detail-label">Gender:</div>
-                <div className="modal-other-detail-value">{person.gender}</div>
-              </div>
-              <div className="modal-other-detail">
-                <div className="modal-other-detail-label">Phone:</div>
-                <div className="modal-other-detail-value">{person.phone}</div>
-              </div>
-              <div className="modal-other-detail">
-                <div className="modal-other-detail-label">Age:</div>
-                <div className="modal-other-detail-value">{person.age}</div>
-              </div>
-              <div className="modal-other-detail">
-                <div className="modal-other-detail-label">Blood Group:</div>
-                <div className="modal-other-detail-value">
-                  {person.bloodGroup}
+            {edit ? (
+              <>
+                <div className="modal-other-detail-edit">
+                  <div className="modal-other-detail-label">Gender:</div>
+                  <Select
+                    className="modal-other-detail-input"
+                    defaultValue={editValues.gender}
+                    style={{ width: "100%" }}
+                    onChange={(e) =>
+                      handleEditValueChange("gender", e.target.value)
+                    }
+                    options={[
+                      { value: "male", label: "Male" },
+                      { value: "female", label: "Female" },
+                    ]}
+                  />
+                </div>
+                <div className="modal-other-detail-edit">
+                  <div className="modal-other-detail-label">Phone:</div>
+                  <Input
+                    className="modal-other-detail-input"
+                    value={editValues.phone}
+                    onChange={(e) =>
+                      handleEditValueChange("phone", e.target.value)
+                    }
+                  />
+                </div>
+                <div className="modal-other-detail-edit">
+                  <div className="modal-other-detail-label">Age:</div>
+                  <Input
+                    className="modal-other-detail-input"
+                    value={editValues.age}
+                    onChange={(e) =>
+                      handleEditValueChange("age", e.target.value)
+                    }
+                  />
+                </div>
+                <div className="modal-other-detail-edit">
+                  <div className="modal-other-detail-label">Blood Group:</div>
+                  <Select
+                    className="modal-other-detail-input"
+                    defaultValue={editValues.bloodGroup}
+                    style={{ width: "100%" }}
+                    onChange={(e) =>
+                      handleEditValueChange("bloodGroup", e.target.value)
+                    }
+                    options={[
+                      { value: "A+", label: "A+" },
+                      { value: "A-", label: "A-" },
+                      { value: "B+", label: "B+" },
+                      { value: "B-", label: "B-" },
+                      { value: "O+", label: "O+" },
+                      { value: "O-", label: "O-" },
+                      { value: "AB+", label: "AB+" },
+                      { value: "AB-", label: "AB-" },
+                    ]}
+                  />
+                </div>
+              </>
+            ) : (
+              <div className="modal-other-details">
+                <div className="modal-other-detail">
+                  <div className="modal-other-detail-label">Gender:</div>
+                  <div className="modal-other-detail-value">
+                    {person.gender}
+                  </div>
+                </div>
+                <div className="modal-other-detail">
+                  <div className="modal-other-detail-label">Phone:</div>
+                  <div className="modal-other-detail-value">{person.phone}</div>
+                </div>
+                <div className="modal-other-detail">
+                  <div className="modal-other-detail-label">Age:</div>
+                  <div className="modal-other-detail-value">{person.age}</div>
+                </div>
+                <div className="modal-other-detail">
+                  <div className="modal-other-detail-label">Blood Group:</div>
+                  <div className="modal-other-detail-value">
+                    {person.bloodGroup}
+                  </div>
                 </div>
               </div>
-            </div>
+            )}
           </div>
         </div>
       </Modal>
